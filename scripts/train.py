5fdc8afa1662072126d257fd2b14e6ce3539eb01
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2021-06-17 13:28:26 +0530

    changed trainign script

--- scripts/train.py
+++ scripts/train.py
@@ -2,6 +2,7 @@
 # https://github.com/nguyenq/jTessBoxEditor/releases/tag/Release-2.3.1 can be used for annotating.
 
 import os
+import subprocess
 srcdir = 'tesseract/data'
 destdir = 'tesseract/trainfiles'
 # Removing all previous trained files.
@@ -21,10 +22,11 @@ boxes = [x for x in files if x.endswith('.box')]
 trainfiles = list(zip(jpgs, boxes))
 
 # generating TR files and unicode charecter extraction
-unicharset = f"unicharset_extractor --output_unicharset {destdir}/unicharset"
+unicharset = f"unicharset_extractor --output_unicharset ../../{destdir}/unicharset "
+unicharset_args = f""
 errorfiles = []
 for image, box in trainfiles:
-    unicharset += f" {os.path.join(srcdir,box)}"
+    unicharset_args += f"{box} "
     if os.path.isfile(f"{destdir}/{image[:-4]}.tr"):
         continue
     try:
@@ -32,33 +34,37 @@ for image, box in trainfiles:
         os.system(f"tesseract {srcdir}/{image} {destdir}/{image[:-4]} nobatch box.train")
     except:
         errorfiles.append((image, box))
-os.system(unicharset)
-
-# Creating font properties file
-with open(f"{destdir}/font_properties", 'w') as f:
-    f.write("ocrb 0 0 0 1 0")
-
-# # Getting all .tr files and training
-output = 'tesseract/trainoutput'
-trfiles = [f for f in os.listdir(destdir) if f.endswith('.tr')]
-mftraining = f"mftraining -F {destdir}/font_properties -U {destdir}/unicharset -O {output}/eng.unicharset -D {output}"
-cntraining = f"cntraining -D {output}"
-for file in trfiles:
-    mftraining += f" {destdir}/{file}"
-    cntraining += f" {destdir}/{file}"
-os.system(mftraining)
-os.system(cntraining)
-
-# # Renaming training files and merging them
-os.chdir(output)
-os.rename('inttemp', 'eng.inttemp')
-os.rename('normproto', 'eng.normproto')
-os.rename('pffmtable', 'eng.pffmtable')
-os.rename('shapetable', 'eng.shapetable')
-os.system(f"combine_tessdata eng.")
+os.chdir(srcdir)
+subprocess.run(unicharset+unicharset_args)
+os.chdir('../../')
 
 # Writing log file
 if len(errorfiles) == 0:
     errorfiles.append(('no', 'Error'))
 with open('tesseract/scripts/logs.txt', 'w') as f:
     f.write('\n'.join('%s %s' % x for x in errorfiles))
+
+
+# Creating font properties file
+with open(f"{destdir}/font_properties", 'w') as f:
+    f.write("ocrb 0 0 0 1 0")
+
+# # Getting all .tr files and training
+output = '../../tesseract/trainoutput'
+trfiles = [f for f in os.listdir(destdir) if f.endswith('.tr')]
+os.chdir(destdir)
+mftraining = f"mftraining -F font_properties -U unicharset -O {output}/eng.unicharset -D {output}"
+cntraining = f"cntraining -D {output}"
+for file in trfiles:
+    mftraining += f" {file}"
+    cntraining += f" {file}"
+subprocess.run(mftraining)
+subprocess.run(cntraining)
+os.chdir('../../')
+# # Renaming training files and merging them
+os.chdir(output[6:])
+os.rename('inttemp', 'eng.inttemp')
+os.rename('normproto', 'eng.normproto')
+os.rename('pffmtable', 'eng.pffmtable')
+os.rename('shapetable', 'eng.shapetable')
+os.system(f"combine_tessdata eng.")
